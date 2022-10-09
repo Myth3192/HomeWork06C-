b8905ddfa32a527fd3844dd0a5d2e557219356f3
VladimirIvanov <ivanovvladimir311992@gmail.com>
2022-10-09 18:32:45 +0300

    Task41 added

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите числа через пробел: ");
+int[] numbers = StringToNum(Console.ReadLine());
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine($"Количество значений больше 0: {sum}");
+
+
+
+int[] StringToNum(string input)
+{
+    
+    int[] numbers = new int [input.Length];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ' ')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
